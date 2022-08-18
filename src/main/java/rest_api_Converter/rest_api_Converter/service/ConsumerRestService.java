d50364d3a61f19aad0544773ebdf8c146bb264c9
Nevo Cohen <nevocohen111@gmail.com>
2022-08-18 22:31:29 +0300

    "update"

--- src/main/java/rest_api_Converter/rest_api_Converter/service/ConsumerRestService.java
+++ src/main/java/rest_api_Converter/rest_api_Converter/service/ConsumerRestService.java
@@ -1,20 +1,11 @@
 package rest_api_Converter.rest_api_Converter.service;
 
-import com.fasterxml.jackson.core.JsonParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 import rest_api_Converter.rest_api_Converter.config.RestConfiguration;
 import rest_api_Converter.rest_api_Converter.model.Converter;
-import rest_api_Converter.rest_api_Converter.model.CustomHttpResponse;
-import rest_api_Converter.rest_api_Converter.model.Result;
-
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class ConsumerRestService {
